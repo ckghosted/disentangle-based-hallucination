ece7879561642148298f6253a60e83176b7e970a
ckghosted <ckghosted@gmail.com>
2020-12-31 17:03:35 +0800

    update all shell scripts

---
+++ script_hal_dfhn_1_10_01_01_10_g01.sh
@@ -0,0 +1,18 @@
+python3 ./script_folder/train_hal.py \
+    --n_way $1 \
+    --n_shot $2 \
+    --n_aug $3 \
+    --n_query_all $4 \
+    --fc_dim $5 \
+    --n_train_class $6 \
+    --extractor_folder $7 \
+    --num_epoch $8 \
+    --result_path . \
+    --hallucinator_name HAL_PN_DFHN_1_10_01_01_10_g01_m$1n$2a$3q$4_ep$8 \
+    --DFHN \
+    --lambda_meta 1.0 \
+    --lambda_recon 10.0 \
+    --lambda_consistency 0.1 \
+    --lambda_consistency_pose 0.1 \
+    --lambda_intra 10.0 \
+    --lambda_gan 0.1
---
+++ script_hal_dfhn_1_5_005_005_5_g01.sh
@@ -0,0 +1,18 @@
+python3 ./script_folder/train_hal.py \
+    --n_way $1 \
+    --n_shot $2 \
+    --n_aug $3 \
+    --n_query_all $4 \
+    --fc_dim $5 \
+    --n_train_class $6 \
+    --extractor_folder $7 \
+    --num_epoch $8 \
+    --result_path . \
+    --hallucinator_name HAL_PN_DFHN_1_5_005_005_5_g01_m$1n$2a$3q$4_ep$8 \
+    --DFHN \
+    --lambda_meta 1.0 \
+    --lambda_recon 5.0 \
+    --lambda_consistency 0.05 \
+    --lambda_consistency_pose 0.05 \
+    --lambda_intra 5.0 \
+    --lambda_gan 0.1
